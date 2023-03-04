8d22d5eb166b757eb78db53210d85f564971e325
feruzbekismoilov <ismoilovferuz0858@gmail.com>
2023-03-04 10:26:18 +0500

    get time in js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const API_PATH = "https://islomapi.uz/api/present/day?region=Toshkent";
+
+const elBomdodTime = document.querySelector(".bomdod-time");
+const elPeshinTime = document.querySelector(".peshin-time");
+const elAsrTime = document.querySelector(".asr-time");
+const elShomTime = document.querySelector(".shom-time");
+const elXuftonTime = document.querySelector(".xufton-time");
+const elNamozday = document.querySelector(".js-day");
+
+
+console.log(API_PATH);
+function getTime(API_PATH){
+   fetch(API_PATH )
+   .then((res)=> res.json())
+   .then((data)=>{
+   elNamozday.textContent = data.weekday;
+   elBomdodTime.textContent = data.times.tong_saharlik;
+   elPeshinTime.textContent = data.times.peshin;
+   elAsrTime.textContent = data.times.asr;
+   elShomTime.textContent = data.times.shom_iftor;
+   elXuftonTime.textContent = data.times.hufton;
+   })
+}
+getTime(API_PATH);
